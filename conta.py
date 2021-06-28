184e20ba2554193fdf823a0c73be27c1ff6843c2
GhaSilva <ghabriel-silva@outlook.com>
2021-06-28 20:27:55 -0300

    Modificando visibilidade dos atributos

--- conta.py
+++ conta.py
@@ -4,19 +4,19 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto... {}" .format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo: R$ {} do titular {}" .format(self.saldo, self.titular))
+        print("Saldo: R$ {} do titular {}" .format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
         self.saldo += valor
 
     def saca(self, valor):
-        if(self.saldo >= valor):
-            self.saldo -= valor
+        if(self.__saldo >= valor):
+            self.__saldo -= valor
         else:
             print("Saldo insuficiente")
